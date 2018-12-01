bae71903c083d66062ad3dfabd05252c8bef6914
Your Name <you@example.com>
2018-12-01 11:09:57 +0800

    Update edit.java

--- app/src/main/java/com/example/qinzhu/todolist/edit.java
+++ app/src/main/java/com/example/qinzhu/todolist/edit.java
@@ -1,5 +1,8 @@
 package com.example.qinzhu.todolist;
 
+import android.app.AlarmManager;
+import android.app.Service;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,8 +11,10 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class edit extends AppCompatActivity {
 
@@ -53,9 +58,7 @@ public class edit extends AppCompatActivity {
             public void onClick(View v){
 
                 todo = ed_todo.getText().toString();
-
                 remark = ed_remark.getText().toString();
-
                 bean1 = new bean(todo,remark);
                 Intent intent = new Intent ();
                 intent.putExtra("data",bean1);
@@ -64,6 +67,49 @@ public class edit extends AppCompatActivity {
 
             }
         });
+
+    Button alarm = findViewById(R.id.alarm);
+
+    alarm.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+                //获取系统当前时间
+
+                Calendar calendar=Calendar.getInstance();
+
+                int hourNow=calendar.get(Calendar.HOUR_OF_DAY);
+
+                int minuteNow=calendar.get(Calendar.MINUTE);
+
+                //弹出时间对话框
+
+                TimePickerDialog timePickerDialog=new TimePickerDialog(edit.this, new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                        //c为改了之后的时间
+
+                        Calendar c=Calendar.getInstance();
+                        c.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        c.set(Calendar.MINUTE,minute);
+                        //bean1.setTime(c);
+
+                    }
+
+                },hourNow,minuteNow,true);
+
+
+
+                timePickerDialog.show();
+
+            }
+
+
+    });
+
 
 
     }
